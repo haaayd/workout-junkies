4ef3e7c9b159ed3eef76dd6e363c2697ff007279
haayd <haydee.sramirez@gmail.com>
2022-06-28 22:28:47 -0700

    profiles index ejs

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,7 +1,16 @@
 import { Profile } from '../models/profile.js'
 
 function index(req, res) {
-
+  Profile.find({})
+  .then(profiles => {
+    res.render('profiles/index', {
+      profiles: profiles
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
 }
 
 export {
